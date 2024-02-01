13aff731d545ff64f4fe204b23d2db1cc9417aa6
nihal-dhore <nihaldhore9@gmail.com>
2024-02-01 12:01:39 +0530

    Developed frontend of WisePay (Payment Application)

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+//import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Heading } from "../Components/Heading";
 import { Subheading } from "../Components/Subheading";
